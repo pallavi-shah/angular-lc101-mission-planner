73fc58a86b3a2f25bf1aeb581129aaf888f523db
Pallavi Shah <emailpallavishah@gmail.com>
2019-12-18 15:27:16 -0600

    studio 3

---
+++ mission-planning-dashboard/src/app/crew/crew.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crew',
+  templateUrl: './crew.component.html',
+  styleUrls: ['./crew.component.css']
+})
+export class CrewComponent implements OnInit {
+
+  inCrew: boolean = false;
+  crew: object[] = [];
+
+  candidates: object[] = [
+    {name: "Sally Ride", photo: 'https://handlers.education.launchcode.org/static/images/sally-ride.jpg'},
+    {name: "Mae Jemison", photo: 'https://handlers.education.launchcode.org/static/images/mae-jemison.jpg'},
+    {name: "Ellen Ochoa", photo: 'https://handlers.education.launchcode.org/static/images/ellen-ochoa.jpg'},
+    {name: "Frederick Gregory", photo: 'https://handlers.education.launchcode.org/static/images/frederick-gregory.jpg'},
+    {name: "Guion Bluford", photo: 'https://handlers.education.launchcode.org/static/images/guion-bluford.jpg'},
+    {name: "Kjell Lindgren", photo: 'https://handlers.education.launchcode.org/static/images/kjell-lindgren.jpg'},
+    {name: "Jeanette Epps", photo: 'https://handlers.education.launchcode.org/static/images/jeanette-epps.jpg'}
+  ];
+
+  constructor() { }
+
+  ngOnInit() { }
+
+  // Code the 'addCrewMember' function here:
+
+  addCrewMember(candidate: object){
+    console.log ( " candidate =" + candidate);
+    if(this.crew.includes(candidate)){
+      let crewindex = this.crew.indexOf(candidate);
+      console.log ( " index of = " + crewindex);
+      this.crew.splice(crewindex, 1);
+    } else {
+        if(this.crew.length<3){
+        this.crew.push(candidate);
+      }
+    }
+  }
+}
